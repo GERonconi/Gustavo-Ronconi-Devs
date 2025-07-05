c52c78f1891fb3f8ad753af4976375bc0fdbe263
Gustavo Ronconi <gerprofessional@hotmail.com>
2025-07-05 13:32:31 -0300

    Se mejoro la pagina para que sea responsive, especialmente el indox.html. Se mejoro el formato de los textos de los acordeones, entre otras cosas. Se agrego el menu hamburgguesa con una imagen relacionada al tema de diseño.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 function cambiarIframe(url) {
     document.getElementById('iframe-marketing').src = url;
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    var menuToggle = document.getElementById('menu-toggle');
+    var navMenu = document.querySelector('nav#main-nav ul');
+    if (menuToggle && navMenu) {
+        menuToggle.addEventListener('click', function() {
+            navMenu.classList.toggle('show');
+        });
+    }
+});
